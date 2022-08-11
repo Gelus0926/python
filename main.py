ba7107db499fb22ad9dbcb9c7436b1675b8475a6
308 <308@academicos.cesde.edu.co>
2022-08-11 07:43:00 -0500

    introduccion python

---
+++ main.py
@@ -0,0 +1,15 @@
+# 1. Datos primitivos
+nombre="Gelus"
+edad=19
+estatura=1.66
+comidafavorita=None
+#print("su nombre es: "+nombre+"tu edad es "+edad)
+
+#2. Datos que entran por el teclado
+numero1=int(input("Digite un numero 1: "))
+print(numero1)
+numero2=int(input("Digite un numero 2: "))
+print(numero2)
+suma=numero1 + numero2
+print(f'la suma de {numero1} y {numero2} es {suma}')
+
